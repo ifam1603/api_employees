ae8b95312cd58175776ac4692151e40769844f14
ifam1603 <luis_antonio1603@hotmail.com>
2024-03-11 22:02:07 -0600

    configurado en puerto 3000

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const employeesRoutes = require('./routes/employeesRoutes')
 const app = express()
-const port = 3001
+const port = 3000
 app.use(express.json())
 app.use('/', employeesRoutes)
 //app.use(require('./routes/employeesRoutes'))
